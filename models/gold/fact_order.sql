962b38d3fb2479dbbf7a84a37e51e809ef8f21a6
shubhamkhandekar249@gmail.com <shubhamkhandekar249@gmail.com>
2026-08-25 18:30:52 +0000

    Add Gold views and data quality tests

--- models/gold/fact_order.sql
+++ models/gold/fact_order.sql
@@ -1,11 +1,15 @@
 {{ config(materialized='view') }}
 
 SELECT
-    ORDER_ID,
-    CUSTOMER_ID,
-    PRODUCT_ID,
-    ORDER_DATE,
-    QUANTITY,
-    ORDER_STATUS
-FROM {{ ref('silver_order') }}
-WHERE ORDER_ID IS NOT NULL
+    o.ORDER_ID,
+    o.CUSTOMER_ID,
+    o.PRODUCT_ID,
+    o.ORDER_DATE,
+    o.QUANTITY,
+    o.ORDER_STATUS,
+    p.PRICE,
+    o.QUANTITY * p.PRICE AS ORDER_AMOUNT
+FROM {{ ref('silver_order') }} o
+LEFT JOIN {{ ref('silver_product') }} p
+    ON o.PRODUCT_ID = p.PRODUCT_ID
+WHERE o.ORDER_ID IS NOT NULL
