f1dd9a7572b50e4ef1eb59c84c37a0733557945f
shubhamkhandekar249@gmail.com <shubhamkhandekar249@gmail.com>
2026-08-26 11:17:25 +0000

    commit

--- models/gold/vw_sales_customer_product.sql
+++ models/gold/vw_sales_customer_product.sql
@@ -1,19 +1,18 @@
 {{ config(materialized='view') }}
 
 SELECT
-    -- Order details
     fo.ORDER_ID,
     fo.ORDER_DATE,
     fo.QUANTITY,
-    fo.TOTAL_AMOUNT,
 
-    -- Customer details
     dc.CUSTOMER_ID,
     dc.CUSTOMER_NAME,
     dc.CITY,
     dc.CUSTOMER_TIER,
+    dc.VALID_FROM,
+    dc.VALID_TO,
+    dc.IS_CURRENT,
 
-    -- Product details
     dp.PRODUCT_ID,
     dp.PRODUCT_NAME,
     dp.CATEGORY,
@@ -21,7 +20,7 @@ SELECT
 
 FROM {{ ref('fact_order') }} AS fo
 
-LEFT JOIN {{ ref('dim_customer') }} AS dc
+LEFT JOIN {{ target.schema }}.DIM_CUSTOMER_SCD2 AS dc
     ON fo.CUSTOMER_ID = dc.CUSTOMER_ID
 
 LEFT JOIN {{ ref('dim_product') }} AS dp
